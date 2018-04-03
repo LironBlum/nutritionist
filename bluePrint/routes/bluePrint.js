9422d85f7225877f6dfd0aea60081afe0f822ce7
mey <mey.akiva@bankleumi.co.il>
2018-04-03 04:18:56 +0300

    added writing to log in blueprint example

--- bluePrint/routes/bluePrint.js
+++ bluePrint/routes/bluePrint.js
@@ -1,8 +1,19 @@
+
+require('../utils/stackTraceInfo');
+const logger = require('../models/logger').logger;
+const location = `directory: ${__dirname}, file: ${ __filename}`; //for logging purposes
+
+
+
 function bluePrintExample(req,res) {
+
+    const msg = `bluePrintExample`;
+    const locationMeta = `${location}, func: ${ __func},line:${ __line}`;
+    logger.info(msg, {'meta': `${locationMeta}`});
+
 	res.status(200).json({ bluePrintExample: 'bluePrintExample'});
   }
-  
+
   module.exports = {
 	bluePrintExample
   };
-  
