a707f27d81a18e53bf96c329986d6ac1ba253f80
mey <ladymeyy@gmail.com>
2018-04-16 16:06:28 +0300

    established test environment

---
+++ bluePrint/test/unitTests/blueprint.test.js
@@ -0,0 +1,31 @@
+"use strict";
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../start');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+
+describe('GET /blueprintexample', function() {
+    describe(' correct request', function () {
+        let error, result;
+        before(function (done) {
+            chai.request(app)
+                .get('/blueprintexample')
+                .set('accept', 'application/json')
+                .end(function (err, res) {
+                    error = err;
+                    result = res;
+                    done();
+                });
+        });
+
+        it('Should not fail', function () {
+            expect(error).to.be.null;
+        });
+        it('Should return status code 200', function () {
+            expect(result).to.have.status(200);
+        });
+
+    });
+});
