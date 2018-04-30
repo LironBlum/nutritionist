e0ed555ae3f43252733a65b2851aa7b398cab08b
mey <ladymeyy@gmail.com>
2018-04-30 11:48:45 +0300

    tests for generatePopulation

--- planner/test/unitTest/algorithms/geneticAlgorithm.test.js
+++ planner/test/unitTest/algorithms/geneticAlgorithm.test.js
@@ -1,19 +1,28 @@
 const expect = require("chai").expect;
-const geneticAlgorithm = require("../../../algorithms/geneticAlgorithm");
+const geneticAlgorithm = require("../../../algTools/geneticAlgorithm");
 const request = require("../../testData/request.json");
 const tstHelp = require("../../testHelpFunctions");
 const chromosomeSize = 6;
-
+const popSize = 6;
 describe("genetic Algorithm", function() {
     describe("test ~ createChromosome", function() {
         it("Create a new chromosome (meal plan) with chromosomeSize of genes (products)", function() {
             const chromosome = geneticAlgorithm.createChromosome(request.body.products,chromosomeSize);
-            expect(chromosome.length).to.equal(chromosomeSize);
-            expect(tstHelp.isUniqInArr(chromosome)).to.be.true;
+            expect(chromosome).to.have.property("genes");
+            expect(chromosome).to.have.property("fitness");
+            expect(chromosome.genes.length).to.equal(chromosomeSize);
+            expect(tstHelp.isUniqInArr(chromosome.genes)).to.be.true;
         });
 
     });
 
+    describe("test ~ generateInitPopulation", function() {
+        it("Create a new Population (meal plan options) with popSize of chromosomes (meal plans) ", function() {
+            const pop = geneticAlgorithm.generateInitPopulation(request.body.products,chromosomeSize, popSize);
+            expect(pop.length).to.equal(popSize);
+        });
+
+    });
 
 });
 
