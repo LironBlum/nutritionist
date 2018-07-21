02ccc9c179ab66164c180f91461944bcb6bd7880
mey <ladymeyy@gmail.com>
2018-07-21 20:09:28 +0300

    dbGateWay : added query to products from DB, map results and return results

--- dbGateWay/models/GetProductNutrients.js
+++ dbGateWay/models/GetProductNutrients.js
@@ -1,19 +1,23 @@
 'use strict';
 require('../utils/stackTraceInfo');
+const _ = require('lodash');
 const logger = require('../models/logger').logger;
 const location = `directory: ${__dirname}, file: ${ __filename}`; //for logging purposes
+const MongoClient = require('mongodb').MongoClient;
 const env = process.env;
+
+const url = `mongodb://${env.MONGO_HOST}:${env.MONGO_PORT}/`;
+const dbName = 'nutritionist';
+const collection = 'nutritionValues';
 
 class GetProductNutrients {
   constructor(req) {
-    this.products = req.body.body.products;
+    this.productsNames = req.body.body.products;
     this.meta = {
       service: 'dbGateWay',
       uuid: req.body.meta.uuid
     };
-
   }
-
 
   /**
    * Execute service
@@ -21,11 +25,64 @@ class GetProductNutrients {
    */
   async execute() {
 
+    let productsFromDB;
+    const db = await MongoClient.connect(url);
+    const nutritionDb = db.db(dbName);
+    try {
+      const dbData = await nutritionDb.collection(collection)
+        .find({ 'name.long': { $in: this.productsNames } }, { 'name.long': 1, nutrients: 1 });
+      productsFromDB = await dbData.toArray();
+    }catch(err){
+      console.log(err); //TODO write err to log
+    } finally {
+      db.close();
+    }
+
+    return this.assembleResponse(productsFromDB);
+  }
 
 
+  assembleResponse(productsFromDB){
+    const resultsArr = [];
+    productsFromDB.forEach(product => {
+      const obj = {
+        id: product._id,
+        name: product.name.long,
+        nutrients: GetProductNutrients.filterNutrient(product.nutrients)
+      };
+      resultsArr.push(obj);
+    });
+
+    return resultsArr;
+  }
+
+
+
+  static filterNutrient(nuts){
+
+    const filtered= [];
+    nuts.forEach(nut => {
+      switch (nut.abbr){
+      case 'PROCNT':
+        filtered.push({ name: 'proteins', value: nut.value });
+        break;
+      case 'FAT':
+        filtered.push({ name: 'fats', value: nut.value });
+        break;
+      case 'CHOCDF':
+        filtered.push({ name: 'carbs', value: nut.value });
+        break;
+      case 'ENERC_KCAL':
+        filtered.push({ name: 'calories', value: nut.value });
+        break;
+      }
+    });
+
+    return filtered;
   }
 
 
 
 }
 module.exports = GetProductNutrients;
+
--- dbGateWay/routes/dbGateWay.js
+++ dbGateWay/routes/dbGateWay.js
@@ -4,16 +4,17 @@ const logger = require('../models/logger').logger;
 const location = `directory: ${__dirname}, file: ${ __filename}`; //for logging purposes
 const GetProductNutrients = require('../models/GetProductNutrients');
 
-function getNutritionValues(req,res) {
+const getNutritionValues = async (req,res) => {
 
   const msg = `incoming dbGateWay request`;
   const locationMeta = `${location}, func: ${ __func},line:${ __line}`;
   logger.info(msg, { 'meta': `${locationMeta}` },{ request: req.body });
 
   const inst = new GetProductNutrients(req);
-  inst.execute();
-  res.status(200).json({ dbGateWay: 'dbGateWay' });
-}
+  const results = await inst.execute();
+  res.status(200).json({ results });
+
+};
 
 module.exports = {
   getNutritionValues
