0558130e49f8d582ef6baab4c836f51fc5517624
mey <ladymeyy@gmail.com>
2018-07-21 20:12:02 +0300

    added request to dbGW & request to planner

--- userData/models/orchestrator.js
+++ userData/models/orchestrator.js
@@ -3,13 +3,14 @@ require('../utils/stackTraceInfo');
 const logger = require('../models/logger').logger;
 const location = `directory: ${__dirname}, file: ${ __filename}`; //for logging purposes
 const uuid4 = require('uuid/v4');
+const _ = require('lodash');
 const { sendHttpRequest } = require('../utils/httpRequestHandler');
 const env = process.env;
 
 class Orchestrator {
   constructor(constrains, products) {
-    this.products = products;
-    this.constrains = {
+    this.productsFromClient = products;
+    this.constraints = {
       maxCalories: constrains.maxCal,
       macroMolecules:
       {
@@ -32,17 +33,34 @@ class Orchestrator {
   async execute() {
     const reqToDbGw = {
       meta: this.meta,
-      body: { products: this.products }
+      body: { products: _.map(this.productsFromClient, 'name') }
     };
-    const productsWithNuts = await this.sendToDbGw(reqToDbGw);
+
+
+    const dbGwResponse = await Orchestrator.sendToDbGw(reqToDbGw);
+
+    const productsFromDB =  dbGwResponse.body.results;
+
+
+    console.log(' ------------------- productsWithNuts ---------------------------', dbGwResponse.body.results);
+
+    const reqToPlanner = this.buildPlannerRequest(productsFromDB);
+
+
+    console.log('-------------------  reqToPlanner --------------', reqToPlanner);
+
+    const plannerResponse =  await Orchestrator.sendToPlanner(reqToPlanner);
+
+
+    console.log('-------------------  plannerResponse --------------', plannerResponse.body);
 
   }
 
   /**
-   * Send request to CCO => credit card orchestrator
+   * Send request to dbGateway
    * @return {Promise<void>}
    */
-  async sendToDbGw(products){
+  static async sendToDbGw(products){
     const url = `${env.DB_GW_PROTOCOL}://${env.DB_GW_HOST}:${env.DB_GW_PORT}${env.DB_GW_URI}`;
 
     const opt = { url, data: products };
@@ -53,5 +71,62 @@ class Orchestrator {
       return err;
     }
   }
+
+
+  static async sendToPlanner(data){
+    const url = `${env.PLANNER_PROTOCOL}://${env.PLANNER_HOST}:${env.PLANNER_PORT}${env.PLANNER_URI}`;
+
+    const opt = { url, data };
+
+    try {
+      return await sendHttpRequest('post', opt);
+    }catch (err) {
+      return err;
+    }
+
+  }
+
+  buildPlannerRequest(productsFromDB){
+    const reqToPlanner = {
+      meta: this.meta,
+      body: { constraints: this.constraints, products: [] }
+    };
+
+    productsFromDB.forEach(product => {
+      const obj = {
+        id: product.id,
+        name: product.name,
+        quantity: parseInt(this.findProductByName(product.name).quantity),
+        units: 'grams'
+      };
+
+      const resolvedNutrients = this.calcNutrientsValueByAmount(product.nutrients, obj.quantity);
+      reqToPlanner.body.products.push(Object.assign(obj, resolvedNutrients));
+    });
+
+    return reqToPlanner;
+  }
+
+
+  findProductByName(name){
+    return this.productsFromClient.find(product => product.name === name);
+  }
+
+  /**
+   * converts nutrition values from 100gr to the input gr
+   * @param nutrientsValuesInGr
+   * @param amountInGr
+   */
+  calcNutrientsValueByAmount(nutrientsValuesInGr, amountInGr ){
+    const results = {};
+    nutrientsValuesInGr.forEach(nutrient => {
+      results[nutrient.name] = (nutrient.value * amountInGr) / 100.0;
+    });
+    return results;
+  }
+
+
+
+
 }
 module.exports = Orchestrator;
--- userData/routes/updateUserData.js
+++ userData/routes/updateUserData.js
@@ -1,4 +1,3 @@
-
 require('../utils/stackTraceInfo');
 const logger = require('../models/logger').logger;
 const location = `directory: ${__dirname}, file: ${ __filename}`; //for logging purposes
@@ -10,20 +9,27 @@ function updateUserData(req,res) {
   const locationMeta = `${location}, func: ${ __func},line:${ __line}`;
   logger.info(msg, { 'meta': `${locationMeta}` });
 
-  //const feData = JSON.parse(Object.keys(req.body)[0]); //input as key becouse of cors problem (find a fix that includes swagger)
+  // const feData = JSON.parse(Object.keys(req.body)[0]); //input as key becouse of cors problem (find a fix that includes swagger)
   const feData = {
     constrains: { maxCal: 1700, carbs: 60, fats: 15, proteins: 20 },
     products:
-    [ { name: 'milk', quantity: '1', unit: 'cup' },
-      { name: 'egg', quantity: '300', unit: 'g' },
-      { name: 'tofu', quantity: '300', unit: 'g' },
-      { name: 'chicken', quantity: '500', unit: 'g' },
-      { name: 'green beans', quantity: '100', unit: 'g' },
-      { name: 'broccoli', quantity: '50', unit: 'g' },
-      { name: 'tuna can', quantity: '100', unit: 'g' },
-      { name: 'rice', quantity: '350', unit: 'g' },
-      { name: 'yogurt', quantity: '300', unit: 'g' },
-      { name: 'h', quantity: 'h', unit: 'h' } ] };
+    [ { name: 'Cheese, cheddar', quantity: '100', unit: 'g' },
+      { name: 'Fish, tuna, light, canned in oil, drained solids', quantity: '100', unit: 'g' },
+      { name: 'Broccoli, raw', quantity: '100', unit: 'g' },
+      { name: 'Tofu, raw, firm, prepared with calcium sulfate', quantity: '100', unit: 'g' },
+      { name: 'Rice, white, long-grain', quantity: '100', unit: 'g' },
+      { name: 'Apples, raw, without skin', quantity: '100', unit: 'g' },
+      { name: 'Chicken breast, roll, oven-roasted', quantity: '100', unit: 'g' },
+      { name: 'Yogurt, plain, low fat', quantity: '100', unit: 'g' },
+      { name: 'Tomatoes, green, raw', quantity: '100', unit: 'g' },
+      { name: 'Carrots, raw', quantity: '100', unit: 'g' },
+      { name: 'Peppers, sweet, green, raw', quantity: '100', unit: 'g' },
+      { name: 'Bread, white, commercially prepared, toasted, low sodium no salt', quantity: '100', unit: 'g' },
+      { name: 'Blueberries, raw', quantity: '100', unit: 'g' },
+      { name: 'Bananas, raw', quantity: '200', unit: 'g' },
+      { name: 'Eggs, scrambled, frozen mixture', quantity: '100', unit: 'g' }] };
+
+
 
   const inst = new Orchestrator(feData.constrains, feData.products);
   inst.execute();
